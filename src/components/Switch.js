69621f5e4020e92aaf1b7449d6315059aeef750c
Devyalamaddi <devendrayalamaddi@gmail.com>
2024-10-22 18:27:51 +0530

    Update Switch.js
    Props Added: Added isChecked and onToggle props to control the switch state and handle the toggle event externally.
    Label Text Removed: Removed the static label text ("label") for a cleaner UI. You can add your desired text by including a prop for it if necessary.

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './Switch.css';
 
-const Switch = () => {
+const Switch = ({ isChecked, onToggle }) => {
   return (
     <>
       <input
         className="react-switch-checkbox"
-        id={`react-switch-new`}
+        id="react-switch-new"
         type="checkbox"
+        checked={isChecked}
+        onChange={onToggle}
       />
       <label
         className="react-switch-label"
-        htmlFor={`react-switch-new`}
-      >label
-        <span className={`react-switch-button`} />
+        htmlFor="react-switch-new"
+      >
+        <span className="react-switch-button" />
       </label>
     </>
   );
